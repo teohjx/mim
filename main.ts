3dbba07dd70527d883754577e7eeef09aa802e18
teohjx <74770851+teohjx@users.noreply.github.com>
2020-11-21 14:10:26 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,81 @@
-basic.showLeds(`
-    . # . # .
-    . # . # .
-    # . . . #
-    . # . # .
-    . . # . .
-    `)
+basic.showNumber(7)
+basic.showNumber(6)
+basic.showNumber(5)
+basic.showNumber(4)
+basic.showNumber(3)
+basic.showNumber(3)
+basic.showNumber(2)
+basic.showNumber(1)
+basic.showNumber(0)
 basic.forever(function () {
-	
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 })
